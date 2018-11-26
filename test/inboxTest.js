abf5d3fd4cbe0eefa66a5d9f73db0b6ce754465c
achernyi <ote1188@gmail.com>
2018-11-26 23:24:05 +0200

    mocha run

--- test/inboxTest.js
+++ test/inboxTest.js
@@ -1,3 +1,38 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
+
+
+
+
+
+
+
+
+
+// const web3 = new Web3(ganache.provider());
+//
+// class Car{
+//     park(){
+//         return "stopped";
+//     }
+//     drive(){
+//         return "drive";
+//     }
+// }
+// let car;
+// beforeEach(() => {
+//     car = new Car();
+//
+//
+// });
+// describe("Car",() => {
+//     it("can park",() => {
+//
+//         assert.strictEqual(car.park(), "stopped");
+//     });
+//     it("can drive",() => {
+//
+//         assert.strictEqual(car.drive(), "drive");
+//     });
+// });
