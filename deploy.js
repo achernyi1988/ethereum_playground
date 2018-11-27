96df289deab0405c74c03cce740b75df9e318e0a
achernyi <achernyi@luxoft.com>
2018-11-27 20:58:55 +0200

    Added deploy

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HWWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const {interface, bytecode} = require("./compile");
+
+const provider = new HWWalletProvider(
+    "velvet weasel owner rookie agree fit cushion like burden dance tragic lock",
+     "https://rinkeby.infura.io/v3/57028538b43b43ba80e3f61b7e6b5390"
+);
+
+console.log("run");
+const web3 = new Web3(provider);
+const deploy = async () => {
+
+    const accounts = await web3.eth.getAccounts();
+
+    console.log("Attempting to deploy from account = ", accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: ["Hi there!"]})
+        .send({gas: "1000000", from: accounts[0]});
+    console.log("Contract deployed to", result.options.address);
+};
+deploy();
--- test/inboxTest.js
+++ test/inboxTest.js
@@ -1,16 +1,52 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require("../compile");
+
+const INITIAL_MESSAGE_NAME = "Hi there"
+
+let accounts;
+let inbox;
+beforeEach(async()=>{
+    //Get a list of all accounts
+
+    accounts = await  web3.eth.getAccounts();
+
+    //Use one of the these accounts to deploy
+    //the contract
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: [INITIAL_MESSAGE_NAME]})
+        .send({from: accounts[0], gas: "1000000"});
+
+})
+
+describe("Inbox", () => {
+    it ("deploy a contract", () => {
+        assert.ok(inbox.options.address);
+    })
+
+    it("has a default message", async () => {
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(INITIAL_MESSAGE_NAME, message );
+
+    })
+    it("modify message", async () => {
+        const newMessage = "a new message";
+        await inbox.methods.setMessage(newMessage).send({ from : accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.strictEqual(newMessage, message );
+
+    })
+})
 
 
 
-
-
-
-
-
-
-// const web3 = new Web3(ganache.provider());
+//
+//
+//
+//
 //
 // class Car{
 //     park(){
